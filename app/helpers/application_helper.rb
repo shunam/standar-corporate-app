c018619cda08292cca1323dd87438004b15d1201
Walden Global Services <walden@walden-laptop.(none)>
2010-12-22 10:52:52 +0700

    no comment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,7 @@ module ApplicationHelper
 
   def get_image(params = {})
     oauth = @access_token.post('/API/user_info', "candidate_profile_id=#{params["candidate_profile_id"]}").body unless params["candidate_profile_id"].blank?
-    oauth = @access_token.post('/API/user_info', "user_id=#{params["user_id"]}").body unless params["user_id"].blank?
-    debugger
-    a=""
-    
+    oauth = @access_token.post('/API/user_info', "user_id=#{params["user_id"]}").body unless params["user_id"].blank?    
     ActiveSupport::JSON.decode(oauth)["results"]["photo"]
   end
 end
