c264ea5da7b367fb084d6f9aaea6615d2a1f1320
Walden Global Services <walden@walden-laptop.(none)>
2010-12-23 17:22:09 +0700

    favorite and like using partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,10 @@ module ApplicationHelper
     oauth = @access_token.post('/API/user_info', "user_id=#{params["user_id"]}").body unless params["user_id"].blank?    
     ActiveSupport::JSON.decode(oauth)["results"]["photo"]
   end
+
+  def timeago(time, options = {})
+    options[:class] ||= "wallTime"
+    options[:style].blank? ? (options[:style] = "color: grey") : (options[:style] = options[:style] + ";color: grey")
+    content_tag(:span, time.to_s, options.merge({:title => time.getutc.iso8601, })) if time
+  end
 end
