9baa3672c0b8e5afde819fa861def89543343b08
Walden Global Services <walden@walden-laptop.(none)>
2011-01-27 13:06:25 +0700

    fix IFRAME

--- config/initializers/fellownation_key.rb
+++ config/initializers/fellownation_key.rb
@@ -2,10 +2,10 @@ if RAILS_ENV == 'development' || RAILS_ENV == 'test' || RAILS_ENV == 'cucumber'
   SITE = 'http://dev.localhost.local:3000'
   FKEY = "LsFhkv0Ukc8fBI9lwbg"
   FSECRET = "htFCtczsru7mRArtlIrX6Wbazhbx1YlrD0pIhwMw"
-  IFRAME_HELPER = "http://localhost.local:3000/iframe_helper.html"
+  IFRAME_HELPER = "http://apps.localhost.local:3000/iframe_helper.html"
 else
   SITE = 'http://fellownation.com'
   FKEY = "zduBQMNOz9pjXgbOWRio"
   FSECRET = "stSDN6YIRsryEE4wumWPdUkqxhVJUP2qZmtHbBXW"
-  IFRAME_HELPER = "http://fellownation.com/iframe_helper.html"
+  IFRAME_HELPER = "http://apps.fellownation.com/iframe_helper.html"
 end
